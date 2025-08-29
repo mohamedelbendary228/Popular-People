6aab4b59c04b2db786823b37567adb7f50f6cfb1
mohamedelbendary <mohamedelbendary228@gmail.com>
2025-08-29 20:16:14 +0300

    Start a new project

---
+++ android/app/src/main/kotlin/com/mab/com/popular_people/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mab.com.popular_people
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Popular People',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Popular",
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+    );
+  }
+}
