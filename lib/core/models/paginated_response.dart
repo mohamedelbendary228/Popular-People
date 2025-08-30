4080d743e2af738024ba69c541aea93f1a865637
mohamedelbendary <mohamedelbendary228@gmail.com>
2025-08-30 19:50:20 +0300

    Add PaginatedResponse model for handling paginated API responses

---
+++ lib/core/models/paginated_response.dart
@@ -0,0 +1,25 @@
+class PaginatedResponse<T> {
+  final int page;
+  final List<T> results;
+  final int totalPages;
+  final int totalResults;
+
+  PaginatedResponse({
+    this.page = 1,
+    this.results = const [],
+    this.totalPages = 1,
+    this.totalResults = 1,
+  });
+
+  factory PaginatedResponse.fromJson(
+    Map<String, dynamic> json, {
+    required List<T> result,
+  }) {
+    return PaginatedResponse<T>(
+      page: json['page'],
+      results: result,
+      totalPages: json['total_pages'] as int,
+      totalResults: json['total_results'] as int,
+    );
+  }
+}
