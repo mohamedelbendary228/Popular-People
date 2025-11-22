14589e1ef17121479e9e2e1a8e0d8aa80f9ad4a4
mohamedelbendary <mohamedelbendary228@gmail.com>
2025-11-22 21:01:12 +0200

    Chnage default themeMode to dart mode

--- lib/core/theme/providers/switch_theme_provider.dart
+++ lib/core/theme/providers/switch_theme_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ThemeNotifier extends StateNotifier<ThemeMode> {
-  ThemeNotifier() : super(ThemeMode.light);
+  ThemeNotifier() : super(ThemeMode.dark);
 
   void toggleTheme() {
     state = state == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
